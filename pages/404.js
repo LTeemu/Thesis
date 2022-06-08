3c954e2aa07c6f4f0fd6d3a57c1766aa726543b5
n9lete00 <n9lete00@students.oamk.fi>
2022-06-08 18:17:45 +0300

    small fix

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const custom404 = () => {
   return (
     <div className='flex flex-col items-center'>
-      <h2>This page doesn't exist</h2>
+      <h2>This page doesn&apos;t exist</h2>
       <div className='relative w-screen max-w-lg'>
         <Image
           src={'/static/images/oops-gbb906866a_1280-removebg-preview-min.png'}
