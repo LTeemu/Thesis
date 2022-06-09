dc6a9243f84e7f741db0d4de1cc8c219524c3d90
n9lete00 <n9lete00@students.oamk.fi>
2022-06-09 18:12:22 +0300

    unnecessary style change

--- pages/page2.js
+++ pages/page2.js
@@ -2,7 +2,7 @@ import ScrollTop from '../components/ScrollTop'
 import Mug from '../components/Mug'
 const page2 = () => {
   return (
-    <div className='h-[calc(100vh_-_66px)] bg-slate-200'>
+    <div className='h-[calc(100vh_-_66px)]'>
       <Mug />
       <ScrollTop />
     </div >
