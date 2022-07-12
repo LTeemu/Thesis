d7162d67021defc7b4142fce4a50483000373575
n9lete00 <n9lete00@students.oamk.fi>
2022-07-13 00:12:13 +0300

    Better transitions

--- components/HamburgerNav.js
+++ components/HamburgerNav.js
@@ -7,8 +7,13 @@ import { links } from '../public/static/data'
 
 gsap.registerPlugin(ScrollToPlugin)
 
-const HamburgerNav = ({ navOpen, setNavOpen }) => {
+const HamburgerNav = ({ navOpen, setNavOpen, handleLink }) => {
   const navTL = useRef()
+
+  const linkPress = (e, href) => {
+    handleLink(e, href)
+    setNavOpen(false)
+  }
 
   useEffect(() => {
     navTL.current = gsap.timeline({ defaults: { ease: Linear } })
@@ -16,21 +21,21 @@ const HamburgerNav = ({ navOpen, setNavOpen }) => {
       .fromTo('#navContainer', { display: 'none' }, { display: 'block' })
       .fromTo('#navContainer', { height: 0 }, { duration: 1.8, height: 'auto' }, '<')
       .fromTo('#linkContainer a', { opacity: 0, y: 10, pointerEvents: 'none' }, { opacity: 1, pointerEvents: 'auto', stagger: 0.3, duration: 0.3, y: 0, ease: Back.easeOut.config(4) }, '<+0.3')
-      .fromTo('#navContainer', { borderTopLeftRadius: 0, borderTopRightRadius: 0 }, { borderTopLeftRadius: '1rem', borderTopRightRadius: '1rem' })
+    //.fromTo('#navContainer', { borderTopLeftRadius: 0, borderTopRightRadius: 0 }, { borderTopLeftRadius: '1rem', borderTopRightRadius: '1rem' })
   }, [])
 
   useEffect(() => {
-    navOpen ? navTL.current.timeScale(1.7).play() : navTL.current.timeScale(3.4).reverse()
+    navOpen ? navTL.current.timeScale(2).play() : navTL.current.timeScale(5).reverse()
   }, [navOpen])
 
   return (
     <>
       <Hamburger navOpen={navOpen} setNavOpen={setNavOpen} />
-      <div id="navContainer" className='z-50 absolute top-[66px] right-6 md:hidden dark:bg-darksecondary bg-slate-200 shadow-[inset_0_0_6px_0px_rgba(0,0,0,0.5)] rounded-b-2xl hidden'>
+      <div id="navContainer" className='z-50 absolute top-[66px] right-6 md:hidden dark:bg-darksecondary bg-slate-200 shadow-[inset_0_0_6px_0px_rgba(0,0,0,0.5)] rounded-2xl hidden'>
         <div id='linkContainer' className="flex flex-col p-4 md:hidden my-children">
           {links.map((link, index) =>
             <Link href={link.href} key={index}>
-              <a title={link.title} className='text-xl font-bold hover:text-[#865125] dark:hover:text-cyan-300'>{link.title}</a>
+              <a title={link.title} onClick={e => linkPress(e, link.href)} className='text-xl font-bold hover:text-[#865125] dark:hover:text-cyan-300'>{link.title}</a>
             </Link>
           )}
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import HamburgerNav from './HamburgerNav'
 import Darkbtn from './Darkbtn'
 import ToggleBorder from './ToggleBorder'
 import Link from 'next/dist/client/link'
 import { links } from '../public/static/data'
+import { useRouter } from 'next/router'
+import { gsap, Power4 } from 'gsap';
 
 const Navbar = () => {
 	const [navOpen, setNavOpen] = useState(false)
+
+	const router = useRouter()
+	const tl = gsap.timeline();
+
+	const handleLink = (e, href) => {
+		e.preventDefault();
+		if (href !== router.asPath) {
+			const tl = gsap.timeline();
+			tl.to('#transition', { opacity: 0, duration: 0.5, ease: Power4.easeOut, onComplete: () => router.push(href) })
+		}
+	}
+
+	useEffect(() => {
+		const routeChangeAnim = () => {
+			tl.to('#transition', {
+				opacity: 1,
+				duration: 0.5,
+				ease: Power4.easeIn,
+			});
+		}
+
+		router.events.on('routeChangeStart', routeChangeAnim);
+
+		return () => {
+			router.events.off('routeChangeStart', routeChangeAnim);
+		};
+	}, [router]);
 
 	return (
 		<div className='flex justify-between h-[50px] my-2 items-center px-6'>
@@ -16,14 +45,14 @@ const Navbar = () => {
 			<div className='flex-row hidden md:flex ml-children'>
 				{links.map((link, index) =>
 					<Link href={link.href} key={index}>
-						<a title={link.title} className='text-xl font-bold hover:text-[#744622] dark:hover:text-cyan-300'>{link.title}</a>
+						<a onClick={e => handleLink(e, link.href)} title={link.title} className='text-xl font-bold hover:text-[#744622] dark:hover:text-cyan-300'>{link.title}</a>
 					</Link>
 				)}
 			</div>
 			<div className='flex items ml-children'>
 				<ToggleBorder />
 				<Darkbtn />
-				<HamburgerNav navOpen={navOpen} setNavOpen={setNavOpen} />
+				<HamburgerNav navOpen={navOpen} setNavOpen={setNavOpen} handleLink={handleLink} />
 			</div>
 		</div>
 	)
